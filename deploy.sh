5166f87a40e075e01e3d64f252c44cc603dcea97
evgcursed <evgcursed@gmail.com>
2026-07-05 04:38:39 +0500

    refactor

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Обновление и перезапуск всего стека на сервере.
-# Ожидает, что репозиторий фронтенда склонирован рядом: ../lobotomy-frontend
-set -e
-cd "$(dirname "$0")"
-git pull
-git -C ../lobotomy-frontend pull
-docker compose up -d --build
